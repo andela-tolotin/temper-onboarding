b9de0aa635298bff955946547164b43707c2260a
Temitope Olotin <temitope.olotin@andela.com>
2019-11-14 09:23:58 +0100

    refactor the chart plotter repository

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,11 +14,6 @@ class HomeController extends Controller
      */
     private $dataReader;
 
-    /**
-     * @var $file
-     */
-    private $file;
-
     public function __construct(DataSourceInterface $dataReader)
     {
         $this->dataReader = $dataReader;
@@ -37,28 +32,10 @@ class HomeController extends Controller
     public function fetchChartData()
     {
         $file = resource_path() . '/assets/documents/temper.csv';
+
         $this->dataReader->readFile($file);
         $this->chartPlotterRepository = new ChartPlotterRepository($this->dataReader);
 
-        return response()->json($this->getChartSeries());
-    }
-
-    /**
-     * This method get the chart series
-     *
-     * @return array chartSeries
-     */
-    protected function getChartSeries()
-    {
-        $chartSeries = [];
-
-        foreach ($this->chartPlotterRepository->getWeeklyOnBoardingPercentages() as $week => $perscentages) {
-            $chartSeries[] =  [
-                'name' => $week,
-                'data' => array_values($perscentages),
-            ];
-        }
-
-        return $chartSeries;
+        return response()->json($this->chartPlotterRepository->getChartSeries());
     }
 }
